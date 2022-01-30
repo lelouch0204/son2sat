5ef21a0ad11296cdd5b9f92b88732fe056275cf2
Nandan Parikh <f20190097@pilani.bits-pilani.ac.in>
2022-01-30 16:38:14 +0530

    updated tensorflow functions

--- utils/config.py
+++ utils/config.py
@@ -9,26 +9,26 @@ import tensorflow as tf
 import time
 
 # Configuration Entries
-tf.flags.DEFINE_string("mode", "train", "Choose one from: evaluate, restore or train.")
-tf.flags.DEFINE_string("name", "auto",
+tf.compat.v1.flags.DEFINE_string("mode", "train", "Choose one from: evaluate, restore or train.")
+tf.compat.v1.flags.DEFINE_string("name", "auto",
                        "Name of the folder to store the files of the experiment. "
                        "Default value is auto, which is an algorithm that automatically generates a name.")
-tf.flags.DEFINE_string("checkpoint_dir", "", "")
-tf.flags.DEFINE_string("evaluate_dir", "", "")
-tf.flags.DEFINE_string("presentation_dir", "", "")
-tf.flags.DEFINE_string("summary_dir", "", "")
-tf.flags.DEFINE_string("weights_file", "vgg16.npz", "Weights for the VGG16 network.")
-tf.flags.DEFINE_integer("batch_size", 8, "Batch size to use for the network.")
-tf.flags.DEFINE_integer("gen_filters", 32, "Parameter that scales the size of the network.")
-tf.flags.DEFINE_integer("input_channels", 1, "Number of channels in the input images.")
-tf.flags.DEFINE_integer("image_height", 128, "Height of the images to run through the network.")
-tf.flags.DEFINE_integer("image_width", 256, "Width of the images to run through the network.")
-tf.flags.DEFINE_integer("max_to_keep", 1, "Maximum number of checkpoints to keep.")
-tf.flags.DEFINE_integer("num_epochs", 100, "Number of epochs to execute the network for.")
-tf.flags.DEFINE_integer("output_channels", 3, "Number of channels in the output images.")
-tf.flags.DEFINE_float("learning_rate", 0.0002, "Initial learning rate for the optimizer.")
+tf.compat.v1.flags.DEFINE_string("checkpoint_dir", "", "")
+tf.compat.v1.flags.DEFINE_string("evaluate_dir", "", "")
+tf.compat.v1.flags.DEFINE_string("presentation_dir", "", "")
+tf.compat.v1.flags.DEFINE_string("summary_dir", "", "")
+tf.compat.v1.flags.DEFINE_string("weights_file", "vgg16.npz", "Weights for the VGG16 network.")
+tf.compat.v1.flags.DEFINE_integer("batch_size", 8, "Batch size to use for the network.")
+tf.compat.v1.flags.DEFINE_integer("gen_filters", 32, "Parameter that scales the size of the network.")
+tf.compat.v1.flags.DEFINE_integer("input_channels", 1, "Number of channels in the input images.")
+tf.compat.v1.flags.DEFINE_integer("image_height", 128, "Height of the images to run through the network.")
+tf.compat.v1.flags.DEFINE_integer("image_width", 256, "Width of the images to run through the network.")
+tf.compat.v1.flags.DEFINE_integer("max_to_keep", 1, "Maximum number of checkpoints to keep.")
+tf.compat.v1.flags.DEFINE_integer("num_epochs", 100, "Number of epochs to execute the network for.")
+tf.compat.v1.flags.DEFINE_integer("output_channels", 3, "Number of channels in the output images.")
+tf.compat.v1.flags.DEFINE_float("learning_rate", 0.0002, "Initial learning rate for the optimizer.")
 
-FLAGS = tf.flags.FLAGS
+FLAGS = tf.compat.v1.flags.FLAGS
 
 
 def process_config():
